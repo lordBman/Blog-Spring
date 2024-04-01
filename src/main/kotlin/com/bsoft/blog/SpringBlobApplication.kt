889ede3969eea93db4dc8c5eb29abfd610956f23
LordBman <okelekelenobel@gmail.com>
2024-04-01 21:05:13 +0100

    project setup

---
+++ src/main/kotlin/com/bsoft/blog/SpringBlobApplication.kt
@@ -0,0 +1,11 @@
+package com.bsoft.blog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBlobApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBlobApplication>(*args)
+}
---
+++ src/test/kotlin/com/bsoft/blog/SpringBlobApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bsoft.blog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBlobApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
