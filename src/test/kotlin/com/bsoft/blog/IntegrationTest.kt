4550fe420eae527ef0da625ee129d4884cfd904e
lordBman <okelekelenobel@gmail.com>
2024-04-02 08:27:29 +0100

    fixed test file error

--- src/test/kotlin/com/bsoft/blog/IntegrationTest.kt
+++ src/test/kotlin/com/bsoft/blog/IntegrationTest.kt
@@ -1,5 +1,13 @@
 package com.bsoft.blog
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
     @Test
