060e24c67dce516a3dda4df3fe87c4177503628f
lordBman <okelekelenobel@gmail.com>
2024-04-06 22:45:33 +0100

    bug encountered with kotlin, I am never doing this again

--- src/main/kotlin/com/bsoft/blog/SpringBlobApplication.kt
+++ src/main/kotlin/com/bsoft/blog/SpringBlobApplication.kt
@@ -1,5 +1,6 @@
 package com.bsoft.blog
 
+import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,7 @@ import org.springframework.boot.runApplication
 class SpringBlobApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringBlobApplication>(*args)
+	runApplication<SpringBlobApplication>(*args){
+		setBannerMode(Banner.Mode.OFF)
+	}
 }
