78dec3715033a8656299180e3ed249c96098a171
lordBman <okelekelenobel@gmail.com>
2024-04-06 22:38:43 +0100

    finished tutorial

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,14 @@ plugins {
 	kotlin("jvm") version "1.9.23"
 	kotlin("plugin.spring") version "1.9.23"
 	kotlin("plugin.jpa") version "1.9.23"
+	kotlin("plugin.allopen") version "1.9.22"
+	kotlin("kapt") version "1.9.22"
+}
+
+allOpen {
+	annotation("jakarta.persistence.Entity")
+	annotation("jakarta.persistence.Embeddable")
+	annotation("jakarta.persistence.MappedSuperclass")
 }
 
 group = "com.bsoft"
@@ -26,8 +34,16 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
 	runtimeOnly("com.h2database:h2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(module = "mockito-core")
+	}
+	testImplementation("org.junit.jupiter:junit-jupiter-api")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+	testImplementation("com.ninja-squad:springmockk:4.0.2")
+
+	kapt("org.springframework.boot:spring-boot-configuration-processor")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/main/kotlin/com/bsoft/blog/BlogConfiguration.kt
@@ -0,0 +1,29 @@
+package com.bsoft.blog
+
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableConfigurationProperties(BlogProperties::class)
+class BlogConfiguration {
+
+    @Bean
+    fun databaseInitializer(userRepository: UserRepository, articleRepository: ArticleRepository) = ApplicationRunner {
+
+        val johnDoe = userRepository.save(User("johnDoe", "John", "Doe"))
+        articleRepository.save(Article(
+            title = "Lorem",
+            headline = "Lorem",
+            content = "dolor sit amet",
+            author = johnDoe
+        ))
+        articleRepository.save(Article(
+            title = "Ipsum",
+            headline = "Ipsum",
+            content = "dolor sit amet",
+            author = johnDoe
+        ))
+    }
+}
